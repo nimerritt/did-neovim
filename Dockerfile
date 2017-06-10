02ef6c3f45d39e56d6fed7cdb8973074afc5fe2e
Nicholas Merritt <nimerritt@gmail.com>
2017-06-10 12:19:15 +0200

    install plugins

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,14 @@ FROM nimerritt/did-base
 
 MAINTAINER Nicholas Merritt <nimerritt@gmail.com>
 
+SHELL ["/bin/bash", "-c"]
+
 RUN pacman -Sy --noconfirm neovim
-COPY scripts/init.vim /scripts
+
+ENV NVIM_PATH="/root/.config/nvim"
+# Copy init.vim
+COPY scripts/init.vim $NVIM_PATH/init.vim
+
+# Install Plugins
 COPY scripts/configure-neovim.sh /scripts
 RUN /scripts/configure-neovim.sh
--- scripts/configure-neovim.sh
+++ scripts/configure-neovim.sh
@@ -1,9 +1,26 @@
+#!/bin/bash
+plugin () {
+  echo "Installing $1"
+  git clone https://github.com/$1 $1
+  echo "set runtimepath^=$PLUGIN_PATH/$1" >> $NVIM_PATH/extra.vim
+}
+
 echo "Creating alias: vim -> nnvim"
 
 cat <<- 'EOF' >> $HOME/.bashrc
   alias vim='nvim'
 EOF
 
-CONFIG_PATH="$HOME/.config/nvim"
-mkdir -p $CONFIG_PATH
-cp /scripts/init.vim $CONFIG_PATH 
+PLUGIN_PATH=$NVIM_PATH/extra
+echo "Installing plugins to $PLUGIN_PATH"
+mkdir -p $PLUGIN_PATH
+cd $PLUGIN_PATH
+
+plugin 'gioele/vim-autoswap'
+plugin 'scrooloose/syntastic'
+plugin 'tpope/vim-fugitive'
+plugin 'airblade/vim-gitgutter'
+
+echo "source $NVIM_PATH/extra.vim" >> "$NVIM_PATH/init.vim"
+
+
