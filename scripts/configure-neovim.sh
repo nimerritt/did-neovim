d0cadfc4eedce2f540451680efa93a2514984b4e
Nicholas Merritt <nimerritt@gmail.com>
2017-06-11 08:37:43 +0200

    Add more plugins and config

--- scripts/configure-neovim.sh
+++ scripts/configure-neovim.sh
@@ -21,6 +21,16 @@ plugin 'scrooloose/syntastic'
 plugin 'tpope/vim-fugitive'
 plugin 'airblade/vim-gitgutter'
 
+plugin 'altercation/vim-colors-solarized'
+cat <<- 'EOF' >> "$NVIM_PATH/extra.vim"
+  " Pretty colors
+  colorscheme solarized
+  set background=dark      
+EOF
+
+plugin 'kien/ctrlp.vim'
+plugin 'scrooloose/nerdtree'
+
 echo "source $NVIM_PATH/extra.vim" >> "$NVIM_PATH/init.vim"
 
 
--- scripts/init.vim
+++ scripts/init.vim
@@ -1,2 +1,16 @@
 " Quick escape
 :imap jk <Esc>
+nmap ; :
+
+" Allow yank to clipboard
+set clipboard=unnamed
+
+" Moving around splits
+nnoremap gh <C-W><C-H>
+nnoremap gj <C-W><C-J>
+nnoremap gk <C-W><C-K>
+nnoremap gl <C-W><C-L>
+
+" Faster quickfix
+map <C-j> :cn<CR>
+map <C-k> :cp<CR>
